61335d27bb9cac17f94b474c5d40af5b142e7ce5
Oleksandra Puravets <o.puravets@levi9.com>
2016-02-12 11:27:11 +0200

    script upd

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function ()  {
 
     $.ajax("https://content.guardianapis.com/search?api-key=test", {
         success: function (response) {
-            response.response.results.map(function (el) {
+            response.response.results.forEach(function (el) {
             var title = el.webTitle;
             var newsLink = el.webUrl;
             $(".news").append("<li> <a href='" + newsLink + "'> "  + title + "</a> </li>");
