ecb325e763bb924b61c47996019a7ec4afa13d41
Oleksandra Puravets <o.puravets@levi9.com>
2016-02-22 16:36:51 +0200

    function getTopNews transferred from global scope

--- script.js
+++ script.js
@@ -1,42 +1,31 @@
-$(document).ready(function ()  {
-getTopNews();
+$(document).ready(function() {
 
-$(".refresh").on('click', function () {
-$(".news").html("");
-getTopNews();  } );
-    
-});
+        function getTopNews() {
+            $.ajax('https://content.guardianapis.com/search?api-key=test', {
+                success: function(response) {
+                    response.response.results.forEach(function(el) {
+                        var title = el.webTitle;
+                        var newsLink = el.webUrl;
+                        $('.news').append("<li> <a href='" + newsLink + "'> " + title + "</a> </li>");
+                    });
 
-function getTopNews() { $.ajax("https://content.guardianapis.com/search?api-key=test", {
-        success: function (response) {
-            response.response.results.forEach(function (el) {
-            var title = el.webTitle;
-            var newsLink = el.webUrl;
-            $(".news").append("<li> <a href='" + newsLink + "'> "  + title + "</a> </li>");
+                },
+
+
+                error: function(response) {
+                    $('.error-response').html("Sorry, we couldn't find news for you. Please try again later.");
+                }
+
+            });
+
+            getTopNews();
+
+            $('.refresh').on('click', function() {
+                $('.news').html('');
+                getTopNews();
+            });
+
         });
 
-        },
 
-                
-        error: function (jqXHR, exception) {
-        var warningMessage = '';
-        if (jqXHR.status === 0) {
-            warningMessage = "Sorry, we couldn't find news for you. Please try again later.";
-        } else if (jqXHR.status == 404) {
-            warningMessage = 'Requested page not found. [404]';
-        } else if (jqXHR.status == 500) {
-            warningMessage = 'Internal Server Error [500].';
-        } else if (exception === 'parsererror') {
-            warningMessage = 'Requested JSON parse failed.';
-        } else if (exception === 'timeout') {
-            warningMessage = 'Time out error.';
-        } else if (exception === 'abort') {
-            warningMessage = 'Ajax request aborted.';
-        } else {
-            warningMessage = 'Uncaught Error.\n' + jqXHR.responseText;
-        }
-        $('.errorResponse').html(warningMessage);
-        }
-
-    });
- }
+}
