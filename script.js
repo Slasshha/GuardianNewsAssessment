12a32c4750e0823dae20c8ec065654588a5b2d2c
Taras Vovk <taras.ua@gmail.com>
2016-02-24 00:55:12 +0200

    Upd

--- script.js
+++ script.js
@@ -1,31 +1,48 @@
-$(document).ready(function() {
 
+
+    $(document).ready(function () {
+        
         function getTopNews() {
+            
             $.ajax('https://content.guardianapis.com/search?api-key=test', {
-                success: function(response) {
-                    response.response.results.forEach(function(el) {
+                success: function (response) {
+                                        response.response.results.forEach(function (el) {
                         var title = el.webTitle;
                         var newsLink = el.webUrl;
-                        $('.news').append("<li> <a href='" + newsLink + "'> " + title + "</a> </li>");
+                        var newsTextLink = el.apiUrl + '?show-blocks=body&api-key=test';                      
+                        
+                        $('.news').append("<li data-text='" + newsTextLink + "'>" + title + "</li>");                      
                     });
 
                 },
 
-
-                error: function(response) {
+                error: function (response) {
                     $('.error-response').html("Sorry, we couldn't find news for you. Please try again later.");
                 }
+            });            
+           
 
+        }
+        
+            function expandText() {
+            $.ajax($(this).data('text'), {
+                success: function (response) {
+                    console.log(response);
+                }
             });
+        }
 
+
+
+        getTopNews();
+
+        $('.refresh').on('click', function () {
+            $('.news').html('');
             getTopNews();
-
-            $('.refresh').on('click', function() {
-                $('.news').html('');
-                getTopNews();
-            });
-
         });
 
+        $('li').on('click', function () {
+            expandText();
+        });
+    });
 
-}
