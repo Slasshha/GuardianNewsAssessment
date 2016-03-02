7cb440de215985a0f5c9a22122070537b926058e
Oleksandra Puravets <o.puravets@levi9.com>
2016-03-02 17:20:10 +0200

    Fixed alignment bug

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
                     var newsLink = el.webUrl;
                     var newsTextLink = el.apiUrl + '?show-blocks=body&api-key=test';                      
                         
-                    $('.news').append("<li data-collapsed = 'true' data-text='" + newsTextLink + "'>  <h3 class=articleTitle>" + title + "</h3> <span class='arrowCollapsed'> </span>  </li>");
+                    $('.news').append("<li data-collapsed = 'true' data-text='" + newsTextLink + "'>  <span class=articleTitle>" + title + "</span> <span class='arrowCollapsed arrow'> </span>  </li>");
 
                 });
             },
@@ -64,7 +64,7 @@ $(document).ready(function () {
                console.log(response);
                 var textSummary = response.response.content.blocks.body[0].bodyTextSummary;
                 var newsLink = response.response.content.webUrl;
-                currentLi.append("<p class=summary>" + textSummary + "<a href='" + newsLink + "'> Read full news </a>  </p>").children('span').removeClass('arrowCollapsed').addClass('arrowExpanded');
+                currentLi.append("<p class=summary>" + textSummary + "<a href='" + newsLink + "'> Read full news </a>  </p>").children('.arrow').removeClass('arrowCollapsed').addClass('arrowExpanded');
                 currentLi.addClass('clicked');
 
                  }
@@ -73,7 +73,7 @@ $(document).ready(function () {
 
         } else {
             currentLi.find('.summary').html('');
-            currentLi.children('span').removeClass('arrowExpanded').addClass('arrowCollapsed');
+            currentLi.children('.arrow').removeClass('arrowExpanded').addClass('arrowCollapsed');
             currentLi.removeClass('clicked');
         }
 
